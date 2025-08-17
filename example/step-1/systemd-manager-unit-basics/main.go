e052b0d0871aff7e856fbdb20b84e465423043e8
Lee Yunjin <gzblues61@daum.net>
2025-08-17 16:43:38 +0900

    update test code of basic units manipulations

--- example/step-1/systemd-manager-unit-basics/main.go
+++ example/step-1/systemd-manager-unit-basics/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
-
+    "time"
 	"github.com/gosuda/gobus/lib/dbus"
 	"github.com/gosuda/gobus/lib/systemd"
 )
@@ -19,17 +19,20 @@ func main() {
 	sysd := systemd.GetSystemd(conn)
 
 	// Fetch all units
-	units, err := sysd.ListUnits()
+	// units, err := sysd.ListUnits()
+    _, err = sysd.ListUnits()
     if err != nil {
         log.Fatalf("[CRITICAL] Error on loading all units: (%v)\n", err)
     }
 
+    /*
 	// Print the basic info of each unit
 	fmt.Println("List of units:")
 	for _, unit := range units {
 		fmt.Printf("Name: %s, Description: %s, LoadState: %s, ActiveState: %s, SubState: %s\n",
 			unit.Name, unit.Description, unit.LoadState, unit.ActiveState, unit.SubState)
 	}
+    */
 	fmt.Println("Filtered Units:")
 	serviceNames := []string{"systemd-journald.service", "network.service"}
 	filteredUnits, err := sysd.ListUnitsByNames(serviceNames)
@@ -62,4 +65,28 @@ func main() {
 	for _, p := range processes {
 		fmt.Printf("PID: %d, Command: %s, Cgroup: %s\n", p.GetPid(), p.GetCommand(), p.GetCgroup())
 	}
+    const signal = 15
+    sysd.KillUnit("cups.service", "main", signal)
+    path, err := sysd.StopUnit("cups.service", "fail")
+    if err != nil {
+        log.Fatalf("Failed to stop cups.service: %v", err)
+    }
+    log.Printf("%s stopped with no error", path)
+    path, err = sysd.StartUnit("cups.service", "fail")
+    if err != nil {
+        log.Fatalf("Failed to start cups.service: %v", err)
+    }
+    log.Printf("Check your %s. Is is running?\n", path)
+    for i := 0; i < 5; i++ {
+        units, err:= sysd.ListUnitsByNames([]string{"cups.service"})
+        if err != nil {
+            log.Fatalf("Failed to get %s, error: (%v)", path, err)
+        }
+        unit := units[len(units)-1]
+
+        log.Printf("========%d sec========", i)
+        log.Printf("%s: activestate: %s", path, unit.ActiveState)
+        log.Printf("%s: substate: %s", path, unit.SubState)
+        time.Sleep(time.Second)
+    }
 }
